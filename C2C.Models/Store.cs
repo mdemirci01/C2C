aa64c99a9085984714bfa5df72247d2fd854d931
SUATHAKAN SOYLU <suatsoylu55@hotmail.com>
2018-09-11 17:32:04 +0300

    StoreServices And Store Model

---
+++ C2C.Models/Store.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace C2C.Models
+{
+    public class Store : BaseEntity
+    {
+        [Display(Name = "Ad")]
+        [StringLength(200)]
+        public string Name { get; set; }
+        [Display(Name = "Bağlantı")]
+        [StringLength(200)]
+        public string Slug { get; set; }
+        [Display(Name = "Logo")]
+        [StringLength(200)]
+        public string Logo { get; set; }
+        [Display(Name = "Açıklama")]
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Display(Name = "Aktif Mi?")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Değerlendirme")]
+        [StringLength(200)]
+        public float Rating { get; set; }
+        [Display(Name = "Adres")]
+        [StringLength(200)]
+        public string Address { get; set; }
+        [Display(Name = "Telefon")]
+        [StringLength(200)]
+        public string Phone { get; set; }
+        [Display(Name = "E-mail")]
+        [StringLength(200)]
+        public string Email { get; set; }
+        [Display(Name = "Mağaza Sahibi")]
+        [StringLength(200)]
+        public string Owner { get; set; }
+        [Display(Name = "İletişim Adı")]
+        [StringLength(200)]
+        public string ContactName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}

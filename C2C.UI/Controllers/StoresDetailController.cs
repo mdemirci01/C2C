29f8c48126346506beeaca810f2c19b6dbdb447e
ismail kayan <raskomll@hotmail.com>
2018-09-11 18:30:25 +0300

    .

---
+++ C2C.UI/Controllers/StoresDetailController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C2C.UI.Controllers
+{
+    public class StoresDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		public IActionResult Edit()
+		{
+			return View();
+		}
+
+		public IActionResult Delete()
+		{
+			return View();
+		}
+
+		public IActionResult Detalis()
+		{
+			return View();
+		}
+    }
+
+}

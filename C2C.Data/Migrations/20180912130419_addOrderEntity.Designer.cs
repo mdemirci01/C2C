8cdc4367853ec69ebf7f210de32e186219a89c37
umut çakar <nuriumut.cakar@hotmail.com>
2018-09-12 16:17:32 +0300

    .

--- C2C.Data/Migrations/20180912130419_addOrderEntity.Designer.cs
+++ C2C.Data/Migrations/20180912130419_addOrderEntity.Designer.cs
@@ -4,14 +4,16 @@ using C2C.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace C2C.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180912130419_addOrderEntity")]
+    partial class addOrderEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- C2C.Models/OrderStatus.cs
+++ C2C.Models/OrderStatus.cs
@@ -1,7 +1,14 @@
-﻿namespace C2C.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C2C.Models
 {
     public enum OrderStatus
     {
-        Dispatched, Delayed, Delivered
+        [Display(Name = "Gönderiliyor")]
+        Dispatched = 1,
+        [Display(Name = "Beklemede")]
+        Delayed =2,
+        [Display(Name = "Teslim Edildi")]
+        Delivered = 3
     }
 }

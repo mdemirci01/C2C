96d1be31e2d12200b3bb8073f5c14a22af163624
ismail kayan <raskomll@hotmail.com>
2018-09-13 17:35:21 +0300

    .

--- C2C.UI/Controllers/ProductsController.cs
+++ C2C.UI/Controllers/ProductsController.cs
@@ -44,10 +44,79 @@ namespace C2C.UI.Controllers
 
             return View(product);
         }
+
 		public IActionResult Create()
 		{
 			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
 			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Name,Slug,ShortDescription,Description,AdditionalInformation,OldPrice,Price,IsPublished,CategoryId,Availibility,Currency,Id,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,IsDeleted,DeletedBy,DeletedAt,IpAddress")] Product product)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(product);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
+
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var product = await _context.Products.FindAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(string id, [Bind("Name,Slug,ShortDescription,Description,AdditionalInformation,OldPrice,Price,IsPublished,CategoryId,Availibility,Currency,Id,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,IsDeleted,DeletedBy,DeletedAt,IpAddress")] Product product)
+		{
+			if (id != product.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(product);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ProductExists(product.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
+
+		private bool ProductExists(string id)
+		{
+			return _context.Products.Any(e => e.Id == id);
 		}
 	}
 }
